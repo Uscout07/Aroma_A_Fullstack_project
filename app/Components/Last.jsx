14e1347d02e645a0cbb02c48fe33d97900a3a0a5
Uscout07 <pantudit007@gmail.com>
2024-12-10 21:12:22 +0530

    assets

--- app/Components/Last.jsx
+++ app/Components/Last.jsx
@@ -39,7 +39,7 @@ export default function Last() {
               className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <button className="text-white bg-purple-900 font-cinzel font-bold border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
+          <button className="text-white bg-purple-900 font-cinzel font-bold py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
             SUBMIT
           </button>
         </div>
