f6e3d2c49e1079ea12ca9800075dba72fece2c89
Uscout07 <pantudit007@gmail.com>
2024-12-10 19:38:17 +0530

    assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Aroma_A_Fullstack_project',
+  assetPrefix: '/Aroma_A_Fullstack_project',
 }
 
 export default nextConfig
