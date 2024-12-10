e669a90bb2146ae85abac2c83ba791804ffe61be
Uscout07 <pantudit007@gmail.com>
2024-12-10 18:20:22 +0530

    Version Management

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('next.js').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // This enables static HTML export
   basePath: '/Aroma_A_Fullstack_project', // Replace with your exact GitHub repo name
   assetPrefix: '/Aroma_A_Fullstack_project', // Same as basePath
 }
 
-module.exports = nextConfig
+export default nextConfig
