d2a893abb2285dbda5fba05d32389fccc520a139
Uscout07 <pantudit007@gmail.com>
2024-12-10 21:36:35 +0530

    assets

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className='w-full bg-white p-5 flex flex-col items-center justify-center'>
-     <img className="md:w-[5%] w-[10%] border-black border-2 mb-2" src="./assets/AROMA_LOGO.png`"/>
+     <img className="md:w-[5%] w-[10%] border-black border-2 mb-2" src="./assets/logo.png"/>
      <p>Website By Udit Pant</p>
      <p>pantudit2004@gmail.com</p>
     </div>
