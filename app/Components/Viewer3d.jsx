b109d44fe26a1cb7ccda4485b9837fc62017cf9c
Uscout07 <pantudit007@gmail.com>
2024-12-11 19:29:35 +0530

    edit to the home

--- app/Components/Viewer3d.jsx
+++ app/Components/Viewer3d.jsx
@@ -146,9 +146,22 @@ const Lavendar = (props) => {
 
   return (
     <div
-      className="CanvasDiv max-md:absolute max-md:flex max-md:flex-col max-md:items-start fixed max-md:mt-[-4] md:top-[50%] md:translate-y-[-50%] left-[50%] translate-x-[-50%] z-20 pointer-events-none h-screen w-screen"
-      ref={canvasDivRef}
-    >
+  className="CanvasDiv 
+    fixed 
+    max-md:absolute 
+    max-md:top-[5vh] 
+    max-md:left-[50%] 
+    max-md:translate-x-[-50%] 
+    md:top-[50%] 
+    md:left-[50%] 
+    md:translate-y-[-50%] 
+    md:translate-x-[-50%] 
+    z-20 
+    pointer-events-none 
+    h-screen 
+    w-screen"
+  ref={canvasDivRef}
+>
       <Canvas
         shadows
         dpr={[1, 1.5]}
